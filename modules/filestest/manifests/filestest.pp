a05af45172f9dfd5c2f14913652a6379c1123c44
moussa taifi <moutai10@gmail.com>
2012-02-10 13:30:41 -0500

    puppet directory tree

--- modules/filestest/manifests/filestest.pp
+++ modules/filestest/manifests/filestest.pp
@@ -1,5 +1,19 @@
 class  filestest{
 	
+	file { "/etc/puppet/utils":
+    ensure => "directory",
+    owner  => "puppet",
+    group  => "puppet",
+    mode   => 750,
+	}
+	
+	file { "/etc/puppet/utils/fileserver-files":
+    ensure => "directory",
+    owner  => "puppet",
+    group  => "puppet",
+    mode   => 750,
+	}
+	
 	file { "/etc/puppet/utils/fileserver-files/fileserver-test-output.txt":
 	source => "puppet:///utils/fileserver-files/fileserver-test.txt",
 	}
