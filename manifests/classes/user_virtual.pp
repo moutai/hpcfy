7ecc143902fbf27ecefb7647a9786b0601d50a4c
moussa taifi <moutai10@gmail.com>
2012-02-15 16:52:45 -0500

    a7 ssh fix

--- manifests/classes/user_virtual.pp
+++ manifests/classes/user_virtual.pp
@@ -21,7 +21,9 @@ class user::virtual
 	require => File["/home/hpcuser"],
 	command => "mkdir /home/hpcuser/.ssh",
 #ssh-keygen -t dsa -P '' -f /home/hpcuser/.ssh/id_dsa; cat /home/hpcuser/.ssh/id_dsa.pub >> /home/hpcuser/.ssh/authorized_keys ; chown -R hpcuser:hpcuser /home/hpcuser/.ssh;",
-	refreshonly => true,
+	#refreshonly => true,
+	path => '/usr/bin:/usr/sbin:/bin',
+    onlyif => 'test ! -d /hpcuser/.ssh'
 	}
 	
 	
