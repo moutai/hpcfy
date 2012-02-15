bebd4bccd1571bfd42dad404f58874fbf4f872b6
moussa taifi <moutai10@gmail.com>
2012-02-15 13:07:20 -0500

    expending virtual users definition

--- manifests/classes/user_virtual.pp
+++ manifests/classes/user_virtual.pp
@@ -1,6 +1,20 @@
 class user::virtual
 {
-	@user { "hpcuser": }
-	@user { "ladmin": }
+	@user { "hpcuser": 
+		ensure => "present",
+		uid => "5001",
+		gid =>"5001",
+		comment =>"hpc user",
+		home => "/home/hpcuser",
+		shell => "bin/bash",
+	}
+	@user { "ladmin": 
+		ensure => "present",
+		uid => "5002",
+		gid =>"5002",
+		comment =>"admin",
+		home => "/home/admin",
+		shell => "bin/bash",
+	}
 	
 }
