a9876593d1dd6f78165b7bf6bb5d27bc16329de0
moussa taifi <moutai10@gmail.com>
2012-02-15 12:38:13 -0500

    q

---
+++ manifests/classes/nfsserver.pp
@@ -0,0 +1,36 @@
+class nfs::server{
+
+	package { "nfs-kernel-server": ensure => installed }
+	
+	service { "nfs-kernel-server":
+	ensure => running,
+	enable => true,
+	hasrestart => true, 
+	require => Package["nfs-kernel-server"],
+	}
+	
+	exec { "update-etc-exports":
+	command => "/bin/cat /etc/exports.d/* >/etc/exports",
+	notify => Service["nfs-kernel-server"],
+	refreshonly => true,
+	}
+	
+	
+	define share( $path, $allowed, $options = "" ) {
+	include nfs::server
+	
+	file { $path:
+		ensure => directory,
+	}
+	
+	file { "/etc/exports.d":
+	ensure => directory,
+	}
+	
+	file { "/etc/exports.d/${name}":
+	content => "${path} ${allowed}(${options})\n",
+	notify => Exec["update-etc-exports"],
+	}
+	}
+	
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,5 @@
-import "nodes"
+import "nodes.pp"
+import "classes/*"
+
 
 
