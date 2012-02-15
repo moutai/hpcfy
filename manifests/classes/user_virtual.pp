ab86247e77a300340f68f6ed06eec8662a796ebb
moussa taifi <moutai10@gmail.com>
2012-02-15 16:44:25 -0500

    a3

--- manifests/classes/user_virtual.pp
+++ manifests/classes/user_virtual.pp
@@ -18,8 +18,8 @@ class user::virtual
 	}
 	
 	exec { "fix the ssh keys for password less internode access":
-	command => "mkdir /home/hpcuser/.ssh;ssh-keygen -t dsa -P '' -f /home/hpcuser/.ssh/id_dsa; cat /home/hpcuser/.ssh/id_dsa.pub >> /home/hpcuser/.ssh/authorized_keys",
 	require => File["/home/hpcuser"],
+	command => "mkdir /home/hpcuser/.ssh;ssh-keygen -t dsa -P '' -f /home/hpcuser/.ssh/id_dsa; cat /home/hpcuser/.ssh/id_dsa.pub >> /home/hpcuser/.ssh/authorized_keys ; chown -R hpcuser:hpcuser /home/hpcuser/.ssh;",
 	refreshonly => true,
 	}
 	
