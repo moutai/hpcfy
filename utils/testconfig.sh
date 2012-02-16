0267802970ce0292f5f7ec03f1e726bde60c3fef
moussa taifi <moutai10@gmail.com>
2012-02-16 11:16:55 -0500

    a1

--- utils/testconfig.sh
+++ utils/testconfig.sh
@@ -7,4 +7,5 @@ pwd;
 #git push; 
 
 
-ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploy; python triggerapplyall.py'; 
+#ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploy; python triggerapplyall.py'; 
+ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploy; rake apply CLIENT=10.80.160.13'; 
