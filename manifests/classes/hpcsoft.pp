bf939c1eb7f0db0393c5f3ed8f2104c7a7edf20a
moussa taifi <moutai10@gmail.com>
2012-02-16 16:09:39 -0500

    as

--- manifests/classes/hpcsoft.pp
+++ manifests/classes/hpcsoft.pp
@@ -3,28 +3,22 @@ class hpcsoft
 	   file { "/etc/apt/source.list.d/lucid-universe.list":
 				source => "puppet:///utils/apt-get-fixes/lucid-universe.list",
 	   }
-	   exec { "aptupdate":
-        command => "apt-get update",      
-        path    => "/usr/local/bin/:/bin/:/sbin",
-    	}
-	   
-	   
 	   	
-	   package { "gcc": ensure => installed }
-	   package { "g++": ensure => installed }
-	   
-	   package { "openmpi-bin":
-	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
-	   }
-		
-	   package { "openmpi-doc":
-	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
-	   }	    
-		
-	   package { "libopenmpi-dev":
-	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
-	   }
+#	   package { "gcc": ensure => installed }
+#	   package { "g++": ensure => installed }
+#	   
+#	   package { "openmpi-bin":
+#	   	ensure => installed,
+#	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
+#	   }
+#		
+#	   package { "openmpi-doc":
+#	   	ensure => installed,
+#	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
+#	   }	    
+#		
+#	   package { "libopenmpi-dev":
+#	   	ensure => installed,
+#	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["aptupdate"]],
+#	   }
 }
