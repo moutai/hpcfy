1ba995c15d5c551d73fccf6c83ad98413e33e1ee
moussa taifi <moutai10@gmail.com>
2012-02-16 14:30:34 -0500

    as

--- manifests/classes/hpcsoft.pp
+++ manifests/classes/hpcsoft.pp
@@ -1,20 +1,25 @@
 class hpcsoft
 {
+	   file { "/etc/apt/source.list.d/lucid-universe.list":
+				source => "puppet:///utils/apt-get-fixes/lucid-universe.list",
+	   }
+	   
+	   	
 	   package { "gcc": ensure => installed }
 	   package { "g++": ensure => installed }
 	   
 	   package { "openmpi-bin":
 	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"]]
+	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["apt-get update"]],
 	   }
 		
 	   package { "openmpi-doc":
 	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"]]
+	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["apt-get update"]],
 	   }	    
 		
 	   package { "libopenmpi-dev":
 	   	ensure => installed,
-	   	require => [Package["gcc"],Package["g++"]]
+	   	require => [Package["gcc"],Package["g++"],File["/etc/apt/source.list.d/lucid-universe.list"],Exec["apt-get update"]],
 	   }
 }
