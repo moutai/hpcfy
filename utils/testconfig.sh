ac00d62d11ce6ecbf8957ef47390741c6dba73e6
moussa taifi <moutai10@gmail.com>
2012-02-22 13:53:36 -0500

    as

--- utils/testconfig.sh
+++ utils/testconfig.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Proper header for a Bash script.
-pwd;
+#pwd;
 #git add ../.;
 
 #git commit -a; 
 #git push; 
 
 
-ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploy; python triggerapplyall.py'; 
+ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploydevelop; python triggerapplyall.py'; 
 #ssh mtaifi@tec.hpc.temple.edu 'cd Puppet-HPC-virtual-cluster-automation/utils; rake deploy; rake apply CLIENT=10.80.160.13'; 
