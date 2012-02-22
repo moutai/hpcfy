e085f9231ec64b953a9203e87edc4ec57950b02f
moussa taifi <moutai10@gmail.com>
2012-02-22 14:02:43 -0500

    as

--- utils/triggerapplyall.py
+++ utils/triggerapplyall.py
@@ -7,7 +7,7 @@ for line in f.readlines():
 	ip=line.strip().rstrip()
 	line=line.split()
 	print "rake apply CLIENT="+line[0]
-	p=subprocess.Popen("rake apply CLIENT="+line[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	p=subprocess.Popen("rake applydevelop CLIENT="+line[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	for line1 in p.stdout.readlines():
         	print line1
 
