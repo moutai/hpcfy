2471eb841b2a31ce0b19e0589704555242ebcc78
moussa taifi <moutai10@gmail.com>
2012-02-22 16:32:43 -0500

    exec fix at the NAS level

--- manifests/classes/nasbenchsoft.pp
+++ manifests/classes/nasbenchsoft.pp
@@ -16,6 +16,7 @@ class nasbenchsoft
         command => "/bin/cd /home/hpcuser; /bin/tar xzvf /home/hpcuser/NPB3.3.1.tar.gz; /bin/chmod -R hpcuser:hpcuser /home/hpcuser/NPB3.3.1;",  
         require => File["/home/hpcuser/NPB3.3.1.tar.gz"],
         creates => "/home/hpcuser/NPB3.3.1", 
+        logoutput => "on_failure",
 	   }	
     	
     	
