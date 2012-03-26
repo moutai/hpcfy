eff6d2cd86b665504d208b8be30945114d4c92b2
moussa taifi <moutai10@gmail.com>
2012-03-26 17:12:37 -0400

    as

--- manifests/classes/hadoop.pp
+++ manifests/classes/hadoop.pp
@@ -4,26 +4,33 @@ class hadoop
 	
 	package { "openjdk-6-jdk": ensure => installed }
 
+	
+
+
 	file { "/opt/hadoop-debs":
 		ensure => directory
+		notify => Exec["getHadoop"],
 	}
 
 # Currently using the deb to install hadoop 
 #http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-1.0.1/hadoop_1.0.1-1_amd64.deb
 
-	file { "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb":
-	    	owner   => root,
-	   	group   => root,
-	   	mode    => 644,
-	    	ensure  => present,
-	    	source  => "http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-1.0.1/hadoop_1.0.1-1_amd64.deb"
+
+	exec { "getHadoop":
+  		provider => shell,
+		cwd =>"/opt/hadoop-debs",
+		command => "wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-1.0.1/hadoop_1.0.1-1_amd64.deb",
+		creates => "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb",
+		logoutput => "on_failure",
 	}
 
-   	package { "hadoop":
-     		provider => dpkg,
-    		ensure => installed,
-    		source => "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb"
-	}
+	
+
+   	#package { "hadoop":
+     	#	provider => dpkg,
+    	#	ensure => installed,
+    	#	source => "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb"
+	#}
 		
 	   
 }
