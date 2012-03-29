12d3a7b8781f28a360bad4fa69128543b0159c68
moussa taifi <moutai10@gmail.com>
2012-03-29 15:27:05 -0400

    adding hadoop config files

--- manifests/classes/hadoop.pp
+++ manifests/classes/hadoop.pp
@@ -1,21 +1,14 @@
 class hadoop
 {
 	Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] } 
-	
 	package { "openjdk-6-jdk": ensure => installed }
-
-	
-
 
 	file { "/opt/hadoop-debs":
 		ensure => directory,
 		notify => Exec["getHadoop"],
 	}
-
 # Currently using the deb to install hadoop 
 #http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-1.0.1/hadoop_1.0.1-1_amd64.deb
-
-
 	exec { "getHadoop":
   		provider => shell,
 		cwd =>"/opt/hadoop-debs",
@@ -23,14 +16,59 @@ class hadoop
 		creates => "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb",
 		logoutput => "on_failure",
 	}
-
-	
-
    	package { "hadoop":
      		provider => dpkg,
     		ensure => installed,
     		source => "/opt/hadoop-debs/hadoop_1.0.1-1_amd64.deb"
 	}
-		
-	   
+
+# Config files
+	file { "/etc/hadoop/hadoop-env.sh":
+				source => "puppet:///utils/fileserver-files/hadoop_conf/hadoop-env.sh",
+				owner=> root,
+        			group => root,
+				require => Package["hadoop"],
+	}
+	
+
+	file { "/etc/hadoop/core-site.xml":
+				source => "puppet:///utils/fileserver-files/hadoop_conf/core-site.xml",
+				owner=> root,
+        			group => root,
+				require => Package["hadoop"],
+	}
+	
+
+	file { "/hadoop_data/tmp":
+		ensure => directory,
+		owner=> hpcuser,
+        	group => hpcuser,
+		require => Package["hadoop"],
+	}
+	
+	#file { "/hadoop_data/tmp":
+	#	ensure => directory,
+	#}
+
+	
+			  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
