c0f75701e580cdb4c0ae6df44243f58b4d212755
moussa taifi <moutai10@gmail.com>
2012-08-06 14:23:55 -0400

    adding the hpcuser as a default login to master

--- utils/logintomaster.sh
+++ utils/logintomaster.sh
@@ -1,1 +1,2 @@
-ssh root@`head -1 hosts | cut -d " " -f1`
+ssh root@`head -1 hosts | cut -d " " -f1` 'cat  /root/.ssh/authorized_keys >> /home/hpcuser/.ssh/authorized_keys'
+ssh hpcuser@`head -1 hosts | cut -d " " -f1`
