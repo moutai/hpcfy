fa8a6bdc5ac7bcd121ef715702b582862cdfd179
moussa taifi <moutai10@gmail.com>
2012-09-20 10:46:57 -0400

    adding the market-hadoop-branch for deploying tests

--- utils/triggerapplyall.py
+++ utils/triggerapplyall.py
@@ -1,42 +1,14 @@
 #! /usr/bin/env python
-
-
 import os
 import subprocess
-
 f = open('hosts', 'r')
-
-#raw_input("Enter the deployment version: (M)aster(stable), (D)evelopement \n")
-versionkey='M'
-
-if versionkey=='M':
-	print 'Applying the stable version of repository'
-	for line in f.readlines():
-		ip=line.strip().rstrip()
-		line=line.split()
-		print "rake apply CLIENT="+line[0]
-		p=subprocess.Popen("rake apply CLIENT="+line[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		for line1 in p.stdout.readlines():
-	        	print line1
-else:
-	print 'Applying the development version of the repository'
-	
-	print "rake deploydevelop"
-	p=subprocess.Popen("rake deploydevelop", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)	
+for line in f.readlines():
+	ip=line.strip().rstrip()
+	line=line.split()
+	print "rake apply CLIENT="+line[0]
+	p=subprocess.Popen("rake apply CLIENT="+line[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	for line1 in p.stdout.readlines():
-		       	print line1	
-
-	for line in f.readlines(): 
-		ip=line.strip().rstrip()
-		line=line.split()
-		print "rake applydevelop CLIENT="+line[0]
-		p=subprocess.Popen("rake applydevelop CLIENT="+line[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		for line1 in p.stdout.readlines():
-		       	print line1
-	
-
+        	print line1
 f.close()
 
 
-#euca-describe-instances | awk -F" " '{print $4}'| grep 10| sort > ips.txt
-#cat ips.txt
