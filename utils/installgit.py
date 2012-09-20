1c027fe2e24806dcaa76f389cfece6a87ef57bd6
moussa taifi <moutai10@gmail.com>
2012-09-20 14:49:45 -0400

    a

--- utils/installgit.py
+++ utils/installgit.py
@@ -5,10 +5,9 @@ f = open('hosts', 'r')
 for line in f.readlines():
 	ip=line.strip().rstrip()
 	line=line.split()
-	if line[1]=="clusternode0":	
-		#install the git core on the clusternode0 with the -y flag on
-		print "ssh root@"+line[0]+" apt-get -y install git-core "
-		p=subprocess.Popen("ssh root@"+line[0]+" apt-get -y install git-core ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		for line1 in p.stdout.readlines():
-			print line1
+	#install the git core on the clusternode0 with the -y flag on
+	print "ssh root@"+line[0]+" apt-get -y install git-core "
+	p=subprocess.Popen("ssh root@"+line[0]+" apt-get -y install git-core ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	for line1 in p.stdout.readlines():
+		print line1
 f.close()
