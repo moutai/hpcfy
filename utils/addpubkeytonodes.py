af626b4caabb0ef7f85b862a21737397edd1684a
moussa taifi <moutai10@gmail.com>
2012-09-22 19:41:06 -0400

    IMPORTANT: Unstable version of addpubkeytonodes. only work with hijacked images.

--- utils/addpubkeytonodes.py
+++ utils/addpubkeytonodes.py
@@ -53,45 +53,24 @@ for line in f.readlines():
 
 	p=pexpect.spawn("scp "+homedir+"/.ssh/authorized_keys2 root@"+line[0]+":/root/.ssh/authorized_keys")
 	i=p.expect([ssh_newkey,'password:',pexpect.EOF])
-	print p.before
+	print "the answer of first expect:"+str(p.before)+ "and the value of the answer is :" + str(i)
+
 	if i==0:
 	    	print "I say yes"
     		p.sendline('yes')
     		i=p.expect([ssh_newkey,'password:',pexpect.EOF])
+		print "the answer of second expect:"+str(p.before) + "and the value of the answer is :" + str(i)	
+
+
 	if i==1:
 		#temporary fix until we get an image with a public key injected in it. 
     		print "I give password for testing only ",
     		p.sendline("password")
-    		p.expect(pexpect.EOF)
+    		#p.expect(pexpect.EOF)
 	elif i==2:
     		print "I either got key or connection timeout"
     		pass
 	print p.before # print out the result
-
-##doing it again to try to fix any problems from before
-
-for line in f.readlines():
-	ip=line.strip().rstrip()
-	line=line.split()
-	print "scp "+homedir+"/.ssh/authorized_keys2 root@"+line[0]+":/root/.ssh/authorized_keys"
-
-	p=pexpect.spawn("scp "+homedir+"/.ssh/authorized_keys2 root@"+line[0]+":/root/.ssh/authorized_keys")
-	i=p.expect([ssh_newkey,'password:',pexpect.EOF])
-	print p.before
-	if i==0:
-	    	print "I say yes"
-    		p.sendline('yes')
-    		i=p.expect([ssh_newkey,'password:',pexpect.EOF])
-	if i==1:
-		#temporary fix until we get an image with a public key injected in it. 
-    		print "I give password for testing only ",
-    		p.sendline("password")
-    		p.expect(pexpect.EOF)
-	elif i==2:
-    		print "I either got key or connection timeout"
-    		pass
-	print p.before # print out the result
-
 
 
 
